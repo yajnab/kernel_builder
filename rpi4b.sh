dee91975f971dfd79274de7a43b493f2215fbee3
yajnab <yajnab@gmail.com>
2025-09-02 00:41:21 +0530

    rpi4b.sh : Update for Raspberry Pi  4B

--- rpi4b.sh
+++ rpi4b.sh
@@ -1,27 +1,32 @@
-# Gluon Kernel Compiler
-red='tput setaf 1'
-green='tput setaf 2'
-yellow='tput setaf 3'
-blue='tput setaf 4'
-violet='tput setaf 5'
-cyan='tput setaf 6'
-white='tput setaf 7'
-normal='tput sgr0'
-bold='setterm -bold'
-date="date"
+#!/bin/bash
+
+# Updated Gluon Kernel Compiler for Raspberry Pi 4B (64-bit)
+
+# Colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+violet=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+normal=$(tput sgr0)
+bold=$(setterm -bold)
+
+# Naming convention for the new kernel
 KERNEL_BUILD="Gluon_Kernel_Raspberry-`date '+%Y-%m-%d---%H-%M'`" 
-echo $1 > VERSION	
-VERSION='cat VERSION'
-$yellow
-TOOLCHAIN='../../../toolchain/linaro/bin/arm-eabi'
+echo $1 > VERSION
+VERSION=$(cat VERSION)
+
+# Variables for the new build environment
+TOOLCHAIN='../../../toolchain/aarch64-linux-gnu'
 MODULES="./../modules"
+
 cd ../
 rm -rf output
-mkdir output
-mkdir output/boot
-mkdir output/boot/overlays
-mkdir output/modules
-mkdir output/modules/lib
+mkdir -p output/boot/overlays
+mkdir -p output/modules/lib
+
 cd raspberry_pi
 $blue
 echo " |========================================================================| "
@@ -58,27 +63,34 @@ echo "Cleaning"
 $violet
 make clean
 make mrproper
+
 $cyan
-echo " Making config"
+echo "Making config"
 $violet
-ARCH=arm CROSS_COMPILE=../../../toolchain/arm-eabi-4.9/bin/arm-eabi- make bcmrpi_gluon_defconfig
+# Use ARCH=arm64 and bcm2711_defconfig for Raspberry Pi 4B
+ARCH=arm64 CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu- make bcm2711_defconfig
 clear
 
 $cyan
-echo "Making the zImage-the real deal"
+echo "Making the Image-the real deal"
 $violet
-time ARCH=arm CROSS_COMPILE=../../../toolchain/arm-eabi-4.9/bin/arm-eabi- make -j64 CONFIG_DEBUG_SECTION_MISMATCH=y
-time ARCH=arm CROSS_COMPILE=../../../toolchain/arm-eabi-4.9/bin/arm-eabi- INSTALL_MOD_PATH=${MODULES} make modules_install -j64
+# Cross-compile the kernel image
+time ARCH=arm64 CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu- make -j$(nproc) CONFIG_DEBUG_SECTION_MISMATCH=y
+
+# Install modules
+time ARCH=arm64 CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu- INSTALL_MOD_PATH=${MODULES} make modules_install -j$(nproc)
+
 echo "Cleaning"
 $violet
 cd ../
 cd tools_pi
 cd mkimage
-./mkknlimg ../../raspberry_pi/arch/arm/boot/zImage ../../output/boot/kernel.img
+# Use the new kernel image name for 64-bit builds on Pi 4
+./mkknlimg ../../raspberry_pi/arch/arm64/boot/Image ../../output/boot/kernel8.img
 cd ../../
-cp raspberry_pi/arch/arm/boot/dts/*.dtb output/boot
-cp raspberry_pi/arch/arm/boot/dts/overlays/*.dtb* output/boot/overlays/
-cp raspberry_pi/arch/arm/boot/dts/overlays/README output/boot/overlays/
+cp raspberry_pi/arch/arm64/boot/dts/broadcom/*.dtb output/boot
+cp raspberry_pi/arch/arm64/boot/dts/overlays/*.dtb* output/boot/overlays/
+cp raspberry_pi/arch/arm64/boot/dts/overlays/README output/boot/overlays/
 clear
 echo " |============================ F.I.N.I.S.H ! =============================|"
 $red
@@ -86,7 +98,7 @@ echo " |==========================Flash it and Enjoy============================
 $blue
 echo " |==========Don't seek readymade goodies, try to make something new=======| "
 $cyan
-echo " |================================yajnab==================================| "
+echo " |==============================Gluon Works===============================| "
 $red
 echo " |================================Credits=================================| "
 echo " |~~~~~~~~~~~~~~~~~~~~~~Dr.Nachiketa Bandyopadhyay(My Father)~~~~~~~~~~~~~| "
@@ -103,5 +115,4 @@ echo " |****************************Yanuar Harry(squadzone)*********************
 echo " |*********************************faux123********************************| "
 echo " |****************************Linux Torvalds(torvalds)********************| "
 echo " |========================================================================| "
-
 $normal
